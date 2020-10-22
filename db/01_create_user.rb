e6b23c6bf1eb4b66489a5754f7e1c21e9035ccbf
doosemavis <moosedavis2011@gmail.com>
2020-10-21 19:01:34 -0500

    Done.

--- db/01_create_user.rb
+++ db/01_create_user.rb
@@ -1,3 +1,11 @@
-def change
-
+class User < ActiveRecord::Migration[5.2]
+    
+    def change
+        create_table :user do |t| 
+            t.string :username
+            t.string :password
+            t.float :balance
+        end 
+    end 
+    
 end 
