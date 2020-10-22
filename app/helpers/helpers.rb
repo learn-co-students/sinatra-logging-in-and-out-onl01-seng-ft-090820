83a0b0943b055eaec973c836ebf88d49134b8bd6
doosemavis <moosedavis2011@gmail.com>
2020-10-22 13:06:10 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,16 @@
 class Helpers
 
-    def self.current_user
-
+    def self.current_user(session)
+        if session(:user_id)
+            true
+        else
+            false
+        end 
     end 
 
     
     def self.is_logged_in?
-
+  
     end 
 
 end
