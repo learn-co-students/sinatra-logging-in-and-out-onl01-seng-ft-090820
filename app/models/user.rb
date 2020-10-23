6ac9669d07de7a325d21b3461d5260f778a96c49
valeriacopleman <valeriacopleman@gmail.com>
2020-10-23 10:45:05 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 
 class User < ActiveRecord::Base
 
+    attr_accessor :username, :password, :balance
+
+    USER = []
+
+    def initialize (username, password, balance)
+        @name= name
+        @password = password
+        @balance = balance
+        USER << self 
+    end
+
+    def self.all
+        USER 
+    end
+
 end
