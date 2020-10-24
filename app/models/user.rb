db2cd170d81ac07e7b6d0b83269300928e802adb
valeriacopleman <valeriacopleman@gmail.com>
2020-10-23 18:15:14 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,5 @@
 
 class User < ActiveRecord::Base
 
-    attr_accessor :username, :password, :balance
-
-    USER = []
-
-    def initialize (username, password, balance)
-        @name= name
-        @password = password
-        @balance = balance
-        USER << self 
-    end
-
-    def self.all
-        USER 
-    end
 
 end
