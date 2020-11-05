5950e5c821af311a244e6f4018b062c81a9739e0
J K <jkim65@buffalo.edu>
2020-11-05 00:40:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    erb :account
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    end
   end
 
   get '/logout' do
